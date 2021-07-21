9c925cf6434fe8ced5d6fd272d984743d0dbbd10
Pedro Avila <pedrojosavila@gmail.com>
2021-07-21 13:11:19 -0400

    Welcome to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="container">
+    <div class="row mb-1">
+        <div class="col-12 text-center">
+            <img src="{{ asset('img/logo_incared.png')}}" alt="" style="width: 300px;">
+        </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
